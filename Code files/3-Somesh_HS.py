54bf0d7be7a9d4d7687bb222ed9db37e4372166d
SomeshRao007 <111784343+SomeshRao007@users.noreply.github.com>
2022-09-06 23:00:59 +0530

    Update 3-Somesh_HS.py

--- Code files/3-Somesh_HS.py
+++ Code files/3-Somesh_HS.py
@@ -7,41 +7,41 @@ def right(i):
 
 
 
-def MAX_HEAPIFY(A,i):
-
+def MAX_HEAPIFY(A,n,i):
+    largest = i
     l = LEFT(i)
     r = right(i)
 
-    if l<=len(A) - 1 and A[l] > A[i]:
+    if l< n and A[i] < A[l]:
         largest = l
-    else: largest = i
-    if l<= len(A)- 1 and A[r]>A[largest]:
+    #else: largest = i
+    if r< n and A[largest]<A[r]:
         largest = r
     if largest != i :
-
+        #swap(A[i],A[largest])
         A[i],A[largest] = A[largest],A[i]
-       # print(A)
 
-        MAX_HEAPIFY(A,largest)
+        MAX_HEAPIFY(A,n,largest)
 
 def BUILD_MAX_HEAP (A):
-   # n = len(A)
-    for i in range (len(A)//2 - 1, 1, -1 ):
-      #  print(A)
-        MAX_HEAPIFY(A,i)
+    n = len(A)
+    for i in range (n//2 , -1, -1 ):
+        MAX_HEAPIFY(A,n,i)
 
 def HEAPSORT(A):
-    #n= len(A)
+    n= len(A)
     BUILD_MAX_HEAP(A)
-    for i in range (len(A) -1,2,-1):
+    for i in range (n - 1,0,-1):
         A[i],A[0] = A[0],A[i]
-        #swap(A[1],A[i])
-       # n = n-1
-        MAX_HEAPIFY(A,i)
-       # print(A)
-        return A
+
+
+
+        MAX_HEAPIFY(A,i,0)
+    return A
 
 A = [12, 11, 13, 5, 6, 7 ]
 print("The original Array is: ", A)
+#n= len(A)
+#ANS=HEAPSORT(A)
 print('The Sorted Array is:   ',HEAPSORT(A))
-
+# it took me long time to find ou the problem sir i made some changes accordingly. It fun altogether 
